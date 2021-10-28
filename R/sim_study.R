c5fc69dc1e30f9feda4fd60fbe8c0429659af7f4
Lauren Hoskovec <lvheck@rams.colostate.edu>
2021-10-28 08:55:04 -0600

    update vignette

--- R/sim_study.R
+++ R/sim_study.R
@@ -1,35 +1,26 @@
-#' Run a simulation study using PG Bayesian multinomial sampler
+#' Run a simulation study using pgmultinom
 #'
 #' @param simnum simulation number 
-#' @param miss_prob proportion of cases with missing outcome data 
 #' @param niter number of MCMC iterations
 #' @param nburn number of burn-in iterations
-#' @param priors list of priors
 #' @param n sample size
-#' @param K number of outcome categories 
-#' @param p number of exposures
-#' @param q number of covariates 
-#' @param covX covariance of exposure data for simulation 
+#' @param miss_prob proportion of cases with missing outcome data 
 #' @param allmiss logical; if TRUE then all outcomes are missing for any case with missing outcome data, default is FALSE 
 #' @param null_scenario logical; if TRUE then exposures and covariates have no an effect on outcome, default is FALSE
 #' @param equal_probs logical; if TRUE the simulation setting is equal probabilities, if FALSE the simulation setting is data probabilities
 #'
 #' @importFrom mvnfast rmvn
 #'
-#' @return summary of simulation results
+#' @return a list with elements 
 #' @export
 #'
 #'
-#'
-sim_study = function(simnum = 1, miss_prob, niter=1000, nburn = 500, priors=NULL, n=1000, K=6,
-                     p = 3, q = 5, covX = diag(3), 
+
+sim_study = function(simnum = 1, niter=1000, nburn = 500, n=1000, miss_prob = 0,
                      allmiss = FALSE, null_scenario = FALSE, equal_probs = FALSE){
   
   df_all = NULL
   df_cc = NULL
-  # n = 1000 # sample size
-  # K = 6 # number of categories
-  
   print(paste("simnum =", simnum))
   set.seed(21 + 129*simnum)
 
@@ -37,12 +28,13 @@ sim_study = function(simnum = 1, miss_prob, niter=1000, nburn = 500, priors=NULL
   ### simulate exposure and covariate data ###
   ############################################
 
-  # simulate x data with true covariance structure 
-  if(p!=dim(covX)[1]) stop("Number of exposures does not match dimension of covX")
-  #p = dim(covX)[1]
-  x = rmvn(n, mu = rep(0,p), sigma = covX) # change the mean to -1 
+  # exposures
+  p = 3
+  covX = matrix(c(1, -0.13, 0.02, -0.13, 1, -0.86, 0.02, -0.86, 1), 3, 3)
+  x = rmvn(n, mu = rep(0,p), sigma = covX) 
 
-  # simple covariates
+  # covariates 
+  q = 5
   w_cov = rmvn(n, mu = rep(0,q), sigma = diag(q))
   w = cbind(1, w_cov)
   q = ncol(w)
@@ -51,19 +43,17 @@ sim_study = function(simnum = 1, miss_prob, niter=1000, nburn = 500, priors=NULL
   ### simulate outcome data ###
   #############################
 
+  K = 6
+  
   ## stick-breaking simulation ##
   if(!null_scenario){
     beta_true = matrix(rnorm(p*(K-1),0,1), ncol = K-1, nrow = p); beta_true # p by K-1
     gamma_true = matrix(rnorm(q*(K-1),0,1), ncol = K-1, nrow = q); gamma_true # q by K-1
     if(equal_probs) gamma_true[1,] = c(-2.8,-2.5,-2.2,-1.3,-0.5) else gamma_true[1,] = c(1.8,0.5,0,0,0) 
-    #gamma_true[1,] = c(1.8,0.5,0,0,0) # data probs not null 
-    #gamma_true[1,] = c(-2.8,-2.5,-2.2,-1.3,-0.5) # equal probs not null
   }else{
     beta_true = matrix(0, ncol = K-1, nrow = p); beta_true # p by K-1
     gamma_true = matrix(0, ncol = K-1, nrow = q); gamma_true # q by K-1
     if(equal_probs) gamma_true[1,] = c(-1.8,-1.5,-1,-.5,-.3) else gamma_true[1,] = c(1.2,0.8,0.6,0.2,0.1)
-    #gamma_true[1,] = c(1.2,0.8,0.6,0.2,0.1) # data probs null 
-    #gamma_true[1,] = c(-1.8,-1.5,-1,-.5,-.3) # equal probs null
   }
 
   xbetaK_true = crossprod(t(x),beta_true); head(xbetaK_true) # n by K-1
@@ -89,15 +79,7 @@ sim_study = function(simnum = 1, miss_prob, niter=1000, nburn = 500, priors=NULL
     if( !(0 %in% colMeans(y, na.rm = TRUE)) ) break
   }
   
-  # which category 
-  ymax = apply(y, 1, FUN = function(i){
-    which(i==1)
-  })
-  
-  allprobs = table(ymax) # save this for each simulation 
-  #allprobs
-
-  # save y
+  # save complete data 
   ycomplete = y
 
   ###################
@@ -121,29 +103,11 @@ sim_study = function(simnum = 1, miss_prob, niter=1000, nburn = 500, priors=NULL
     y[whichmiss, ] = rep(NA, K)
   }
   
-  # key to which ones are missing 
-  ymiss = matrix(as.numeric(is.na(y)), nrow = n) # save ymiss 
-  #length(which(is.na(y)))
-  
-  ycomplete_k = sapply(1:n, FUN = function(i){
-    which(ycomplete[i,]==1)
-  })
-  
-  y_save = y # save the missing pattern 
-  
-  #################################
-  ### investigating missingness ###
-  #################################
-  
-  # level of missing within each category 
-  head(ymiss)
-  colMeans(ymiss) 
-  
   #################
   ### Fit Model ###
   #################
   
-  fit = pgmultinom(niter = niter, priors = priors, y = y, ycomplete = ycomplete, x = x, w = w,
+  fit = pgmultinom(niter = niter, priors = NULL, y = y, ycomplete = ycomplete, x = x, w = w,
                    intercept = TRUE, beta_true = beta_true, gamma_true = gamma_true)
   
   
@@ -153,61 +117,21 @@ sim_study = function(simnum = 1, miss_prob, niter=1000, nburn = 500, priors=NULL
     x_cc = x[-whichmiss,]
     w_cc = w[-whichmiss,]
 
-    fit_cc = pgmultinom(niter = niter, priors = priors, y = y_cc, ycomplete = NULL, x = x_cc, w = w_cc,
+    fit_cc = pgmultinom(niter = niter, priors = NULL, y = y_cc, ycomplete = NULL, x = x_cc, w = w_cc,
                         intercept = TRUE, beta_true = beta_true, gamma_true = gamma_true)
   }
 
-  
-  
+
   ###################
   ### Imputations ###
   ###################
   
   if(miss_prob > 0){
-    pscore = matrix(unlist(lapply((nburn + 1):niter, FUN = function(s){
-      apply(fit$y.save[[s]][whichmiss,], 1, FUN = function(i){
-        which(i==1)
-      })
-    })), nrow = niter-nburn, ncol = nmiss, byrow = TRUE)
-    
-    # each row is an iteration
-    # each column is a case with missing outcome data 
-    # values are the category assigned 
-    
-    # posterior probability of assignment to each cluster, propensity score 
-    pprobs = t(sapply(1:nmiss, FUN = function(s){
-      cat = pscore[,s]
-      pcat = numeric(K)
-      for(k in 1:K){
-        pcat[k] = length(which(cat==k))/(niter-nburn)
-      }
-      return(pcat)
-    }))
-    
-    # 0-1 loss
-    # consider only the outcomes that are uncertain 
-    # loss of true values will be 0 so divide by length(which(is.na(y)))
-    #zero_one_loss = sum(abs(pprobs - ycomplete[whichmiss, ]))/length(which(is.na(y))) 
-    #zero_one_loss = sum(abs(pprobs - ycomplete[whichmiss, ]))/length(y) 
-    
-    # 0-1 loss for each category 
-    zero_one_loss = numeric(K)
-    for(k in 1:K){
-      zero_one_loss[k] = sum(abs(pprobs[,k] - ycomplete[whichmiss, k]))/length(which(ymiss[,k]==1))
-    }
-
-    # posterior mean classification evaluations 
-    accuracy_mean = apply(fit$accuracy[(nburn+1):niter, ], 2, FUN = function(m) mean(m, na.rm = TRUE))
     precision_mean = apply(fit$precision[(nburn+1):niter, ], 2, FUN = function(m) mean(m, na.rm = TRUE))
     recall_mean = apply(fit$recall[(nburn+1):niter, ], 2, FUN = function(m) mean(m, na.rm = TRUE))
-    specificity_mean = apply(fit$specificity[(nburn+1):niter, ], 2, FUN = function(m) mean(m, na.rm = TRUE))
-  
   }else{
-    zero_one_loss = rep(NA, K)
-    accuracy_mean = rep(NA, K)
     precision_mean = rep(NA, K)
     recall_mean = rep(NA, K)
-    specificity_mean = rep(NA, K)
   }
   
   ########################################
@@ -219,7 +143,6 @@ sim_study = function(simnum = 1, miss_prob, niter=1000, nburn = 500, priors=NULL
   
   bias_beta = mean(fit$bias_beta[(nburn+1):niter])
   bias_gamma = mean(fit$bias_gamma[(nburn+1):niter]) 
-  
   
   beta_quants = t(apply(fit$beta.vec[(nburn+1):niter,], 2, FUN = function(b){
     return(c(quantile(b, 0.025), quantile(b, 0.975)))
@@ -233,30 +156,18 @@ sim_study = function(simnum = 1, miss_prob, niter=1000, nburn = 500, priors=NULL
   wid_gamma = mean(gamma_quants[,2]-gamma_quants[,1])
   cov_gamma = mean(as.vector(gamma_true) > gamma_quants[,1] & as.vector(gamma_true) < gamma_quants[,2])
 
-  
-  # par(mfrow = c(3,5))
-  # for(b in 1:15){
-  #   plot(fit$beta.vec[,b], type = "l")
-  #   abline(h = as.vector(beta_true)[b], col = "red", lwd = 3)
-  # }
-
   #############################
   ### save table of results ###
   #############################
   
-  df1 = data.frame(rbind(c(zero_one_loss, 
-                          rmse_beta, bias_beta, wid_beta, cov_beta, 
+  df1 = data.frame(rbind(c(rmse_beta, bias_beta, wid_beta, cov_beta, 
                           rmse_gamma, bias_gamma, wid_gamma, cov_gamma,
-                          accuracy_mean, precision_mean,
-                          recall_mean, specificity_mean)))
+                          precision_mean, recall_mean)))
 
-  colnames(df1) = c("binary_loss1", "binary_loss2", "binary_loss3", "binary_loss4", "binary_loss5", "binary_loss6", 
-             "RMSE_beta", "bias_beta", "ci_width_beta", "cov_beta", 
+  colnames(df1) = c("RMSE_beta", "bias_beta", "ci_width_beta", "cov_beta", 
              "RMSE_gamma", "bias_gamma", "ci_width_gamma", "cov_gamma", 
-             "accuracy1","accuracy2", "accuracy3","accuracy4","accuracy5","accuracy6",
              "precision1", "precision2", "precision3", "precision4", "precision5", "precision6", 
-             "recall1","recall2","recall3","recall4","recall5","recall6", 
-             "specificity1","specificity2","specificity3","specificity4","specificity5","specificity6")
+             "recall1","recall2","recall3","recall4","recall5","recall6")
   
   df_all = rbind(df_all, df1)
   
@@ -264,7 +175,6 @@ sim_study = function(simnum = 1, miss_prob, niter=1000, nburn = 500, priors=NULL
   ######################################################
   ### Complete Case regression coefficient estimates ###
   ######################################################
-  
 
   if(miss_prob > 0){
     cc_rmse_beta = mean(fit_cc$rmse_beta[(nburn+1):niter])
